3710b8fdf8f8c8b979da833b2469da0bfb399c49
Teodor Draganov <draganov93@gmail.com>
2014-06-20 17:50:19 +0300

    Add separate class for getting playlists information

---
+++ youtube_syncer/playlist_info.py
@@ -0,0 +1,88 @@
+import re
+
+import pafy
+from bs4 import BeautifulSoup
+
+
+class PlaylistInfo():
+    format_and_quality = {
+        "normal": [],
+        "video": [],
+        "audio": []
+    }
+    domain_name = 'https://www.youtube.com'
+
+    @classmethod
+    def _to_int(cls, string):
+        return int(re.sub(r'[^0-9]', '', string))
+
+    @classmethod
+    def _get_available_formats_and_quality(cls, playlists):
+        format_and_quality = cls.format_and_quality
+        for videos in playlists.values():
+            for video in videos:
+                for stream in video['streams'].allstreams:
+                    _format, _quality = str(stream).split(":")
+                    if _quality not in format_and_quality[_format]:
+                        format_and_quality[_format].append(_quality)
+
+        for streams in format_and_quality.values():
+            streams.sort()
+
+        return format_and_quality
+
+    @classmethod
+    def _get_filtered_video_info(cls, video):
+        try:
+            meta_info = video['playlist_meta']
+            video_info = {
+                "title": meta_info['title'],
+                "likes": meta_info['likes'],
+                "dislikes": meta_info['dislikes'],
+                "views": cls._to_int(meta_info['views']),
+                "upload_date": meta_info['added'],
+                "comments_count": cls._to_int(meta_info['comments']),
+                "length_seconds": meta_info['length_seconds'],
+                "rating": meta_info['rating'],
+                "streams": video['pafy'],
+                "selected_stream": None
+            }
+        except:
+            raise
+
+        return video_info
+
+    @classmethod
+    def get_playlists_urls(cls, url_to_playlists_page):
+        page_content = urllib.request.urlopen(url_to_playlists_page).read()
+        soup_obj = BeautifulSoup(page_content)
+        playlists_urls = []
+        for h3_tag in soup_obj.findAll('h3', {'class': 'yt-lockup-title'}):
+            for a_tag in h3_tag.findAll('a', {'class': 'yt-uix-tile-link'}):
+                playlists_urls.append(cls.domain_name+a_tag['href'])
+
+        return playlists_urls
+
+    @classmethod
+    def get_playlist_videos_info(cls, url_to_playlist_page):
+        playlists = {}
+
+        pafy_playlist_info = pafy.get_playlist(url_to_playlist_page)
+        playlist_title = pafy_playlist_info['title']
+
+        playlists[playlist_title] = []
+        for video in pafy_playlist_info['items']:
+            try:
+                video_info = cls._get_filtered_video_info(video)
+                playlists[playlist_title].append(video_info)
+
+            except IOError as message:
+                title = '[ ' + video["playlist_meta"]["title"] + ' ]'
+                message = re.sub(r'\[.+\]', title, str(message))
+                print(message)
+
+            except:
+                print(sys.exc_info()[0])
+
+        return playlists
+
--- youtube_syncer/youtube_syncer.py
+++ youtube_syncer/youtube_syncer.py
@@ -4,9 +4,9 @@ import re
 import urllib.request
 
 import pafy
-from bs4 import BeautifulSoup
 
 from filters import Filter
+from playlist_info import PlaylistInfo
 
 
 class YTSyncer():
@@ -29,96 +29,26 @@ class YTSyncer():
         "stream_format": None,
         "stream_quality": None
     }
-    format_and_quality = {
-        "normal": [],
-        "video": [],
-        "audio": []
-    }
+    format_and_quality = PlaylistInfo.format_and_quality
 
     def __init__(self, url=None):
         if url is not None:
             self.load_playlists(url)
 
-    def _to_int(self, string):
-        return int(re.sub(r'[^0-9]', '', string))
-
-    def _get_available_format_and_quality(self, playlists):
-        for videos in playlists.values():
-            for video in videos:
-                for stream in video['streams'].allstreams:
-                    _format, _quality = str(stream).split(":")
-                    if _quality not in self.format_and_quality[_format]:
-                        self.format_and_quality[_format].append(_quality)
-
-        for streams in self.format_and_quality.values():
-            streams.sort()
-
-    def _get_filtered_video_info(self, video):
-        try:
-            meta_info = video['playlist_meta']
-            video_info = {
-                "title": meta_info['title'],
-                "likes": meta_info['likes'],
-                "dislikes": meta_info['dislikes'],
-                "views": self._to_int(meta_info['views']),
-                "upload_date": meta_info['added'],
-                "comments_count": self._to_int(meta_info['comments']),
-                "length_seconds": meta_info['length_seconds'],
-                "rating": meta_info['rating'],
-                "streams": video['pafy'],
-                "selected_stream": None
-            }
-        except:
-            raise
-
-        return video_info
-
-    def get_playlists_urls(self, url_to_playlists_page):
-        page_content = urllib.request.urlopen(url_to_playlists_page).read()
-        soup_obj = BeautifulSoup(page_content)
-        playlists_urls = []
-        domain_name = 'https://www.youtube.com'
-        for h3_tag in soup_obj.findAll('h3', {'class': 'yt-lockup-title'}):
-            for a_tag in h3_tag.findAll('a', {'class': 'yt-uix-tile-link'}):
-                playlists_urls.append(domain_name+a_tag['href'])
-
-        return playlists_urls
-
-    def get_playlist_videos_info(self, url_to_playlist_page):
-        playlists = {}
-
-        pafy_playlist_info = pafy.get_playlist(url_to_playlist_page)
-        playlist_title = pafy_playlist_info['title']
-
-        playlists[playlist_title] = []
-        for video in pafy_playlist_info['items']:
-            try:
-                video_info = self._get_filtered_video_info(video)
-                playlists[playlist_title].append(video_info)
-
-            except IOError as message:
-                title = '[ ' + video["playlist_meta"]["title"] + ' ]'
-                message = re.sub(r'\[.+\]', title, str(message))
-                print(message)
-
-            except:
-                print(sys.exc_info()[0])
-
-        return playlists
-
     def load_playlists(self, url):
         playlists = {}
         if url.find('playlists') > 0:
-            playlists_urls = self.get_playlists_urls(url)
+            playlists_urls = PlaylistInfo.get_playlists_urls(url)
         else:
             playlists_urls = [url]
 
         for playlist_url in playlists_urls:
-            playlist_info = self.get_playlist_videos_info(playlist_url)
+            playlist_info = PlaylistInfo.get_playlist_videos_info(playlist_url)
             playlists = \
                 dict(list(playlists.items()) + list(playlist_info.items()))
 
-        self._get_available_format_and_quality(playlists)
+        self.format_and_quality = \
+            PlaylistInfo._get_available_formats_and_quality(playlists)
         self.playlists = playlists
 
     def filter_videos(self, selected=None, min_likes=None, max_likes=None,
